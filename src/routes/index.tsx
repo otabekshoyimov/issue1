1157bc2b4946459252be0d9e0eb82bab79acd3a3
otabek <otabek_s28@icloud.com>
2024-07-12 09:35:13 +0500

    refactor and fix: LoB, fix navbar event handler

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import { Outlet, useOutletContext } from 'react-router-dom';
+import React, { ReactNode, useState } from 'react';
+import { Link, Outlet, useOutletContext } from 'react-router-dom';
 import { useIssueContext } from './issueContext';
 import { useAddIssue, useDeleteIssues, useIssues } from '../api/api';
 import { OutletContext } from './root';
 import { Dialog } from './shared/components/dialog';
-import { Issue } from './shared/components/issue';
-import { IssuesList } from './shared/components/issues-list';
-import { IssuesHeader } from './shared/components/issues-header';
-import { IssuesListHeader } from './shared/components/issues-list-header';
-import { IssuesContainer } from './shared/components/issues-container';
+import { OpenNavSVG } from '../shared/components/svgs/open.nav';
+import { BacklogSVG } from '../shared/components/svgs/backlog-svg';
+import { TodoSVG } from '../shared/components/svgs/todo-svg';
+import { InProgressSVG } from '../shared/components/svgs/in-progess';
+import { DoneSVG } from '../shared/components/svgs/done';
+import { CancelledSVG } from '../shared/components/svgs/cancelled';
+import {
+  Button as ReactAriaButton,
+  Key,
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select,
+  SelectValue,
+} from 'react-aria-components';
+import { Button } from './shared/components/button';
 
 export type NewIssue = {
   id: string;
@@ -46,6 +57,7 @@ export const Index = () => {
         if (outletContext.dialogRef.current) {
           outletContext.dialogRef.current.close();
         }
+        outletContext.setIsNavVisible(false);
         issuesAsync.refetch();
       },
     });
@@ -103,3 +115,182 @@ export const dateFormatter = new Intl.DateTimeFormat('en-US', {
   day: '2-digit',
   month: 'short',
 });
+
+type IssuesContainerProps = {
+  children: ReactNode;
+};
+
+const IssuesContainer = (props: IssuesContainerProps) => {
+  return (
+    <>
+      <main className="bg-[#fbfbfb] mb-2 rounded-md">{props.children}</main>
+    </>
+  );
+};
+
+type IssuesHeaderProps = {
+  handleSelecetedIssue: () => void;
+  selectedIssues: string[];
+};
+const IssuesHeader = (props: IssuesHeaderProps) => {
+  const outletContext = useOutletContext<OutletContext>();
+  return (
+    <>
+      <header className="  text-sm border-0 border-b border-solid border-gray-300 ">
+        <div className="flex px-4 h-9 gap-10 ">
+          <div className="flex items-center justify-center gap-10">
+            <button
+              onClick={() => {
+                outletContext.handleIsNavVisibleClick();
+              }}
+              className="flex items-center hover:bg-gray-300 p-1 rounded-md nav-btn z-[97] relative lg:hidden"
+            >
+              <OpenNavSVG name="OpenNav" width={16} height={16} />
+            </button>
+            <span>All issues</span>
+          </div>
+          <div className="flex-grow justify-end flex items-center">
+            <Button
+              text="Delete"
+              onClick={props.handleSelecetedIssue}
+              disabled={props.selectedIssues.length === 0}
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+const IssuesListHeader = () => {
+  return (
+    <>
+      <div className="px-5 py-2 bg-gray-100 border-0 border-b border-solid border-gray-300 flex items-center gap-2 text-sm">
+        <BacklogSVG name="Backlog" width={14} height={14} /> Backlog
+      </div>
+    </>
+  );
+};
+
+type IssuesListProps = {
+  children: ReactNode;
+};
+const IssuesList = (props: IssuesListProps) => {
+  return (
+    <>
+      <main className="border-0 border-b border-solid border-gray-300 ">
+        {props.children}
+      </main>
+    </>
+  );
+};
+
+//#region MARK: Issue
+
+type IssueProps = {
+  issue: NewIssue;
+  selectedIssues: string[];
+  handleIssueSelection: (issueId: string) => void;
+};
+const Issue = (props: IssueProps) => {
+  // Define a type for your items
+  type ItemType = {
+    key: string;
+    text: string;
+    icon: React.ReactNode;
+  };
+
+  const items: ItemType[] = [
+    {
+      key: 'Backlog',
+      text: 'Backlog',
+      icon: <BacklogSVG name="Backlog" width={14} height={14} />,
+    },
+    {
+      key: 'Todo',
+      text: 'Todo',
+      icon: <TodoSVG name="Todo" width={14} height={14} />,
+    },
+    {
+      key: 'InProgress',
+      text: 'In Progress',
+      icon: <InProgressSVG name="InProgress" width={14} height={14} />,
+    },
+    {
+      key: 'Done',
+      text: 'Done',
+      icon: <DoneSVG name="Done" width={14} height={14} />,
+    },
+    {
+      key: 'Cancelled',
+      text: 'Cancelled',
+      icon: <CancelledSVG name="Cancelled" width={14} height={14} />,
+    },
+  ];
+  const [selectedKey, setSelectedKey] = useState<Key>('Backlog');
+  const selectedItem = items.find((item) => item.key === selectedKey);
+
+  return (
+    <>
+      <li className="hover:bg-[#e1e1e1] hover:rounded-md flex gap-4 px-5 border-0 border-solid border-b border-gray-300  leading-8 items-center">
+        <input
+          type="checkbox"
+          value={props.issue.checked.toString()}
+          checked={props.selectedIssues.includes(props.issue.id)}
+          onChange={() => props.handleIssueSelection(props.issue.id)}
+        />
+
+        <Link
+          className="flex justify-between flex-grow"
+          to={`${props.issue.id}`}
+        >
+          <Select
+            defaultSelectedKey={selectedKey}
+            onSelectionChange={(selected) => setSelectedKey(selected)}
+            className="flex  gap-1 w-fit pr-3"
+          >
+            <ReactAriaButton>
+              <SelectValue className="flex items-center gap-2">
+                {selectedItem && (
+                  <>
+                    <span className="flex gap-2 items-center">
+                      {selectedItem.icon}
+                    </span>
+                    <span className="text-sm">{selectedItem.text}</span>
+                  </>
+                )}
+              </SelectValue>
+            </ReactAriaButton>
+            <Popover
+              placement="right top"
+              className="overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 data-[entering]:animate-in entering:fade-in exiting:animate-out exiting:fade-out"
+            >
+              <ListBox
+                className="flex flex-col gap-2 items-center"
+                items={items}
+              >
+                {items.map((item) => (
+                  <ListBoxItem
+                    key={item.key}
+                    className="px-2 data-[selected]:bg-blue-400 data-[disabled]:bg-gray-100 data-[focused]:bg-gray-200 flex gap-2 items-center"
+                  >
+                    {item.icon}
+                    {item.text}
+                  </ListBoxItem>
+                ))}
+              </ListBox>
+            </Popover>
+          </Select>
+          <div className="flex-grow justify-between flex items-center">
+            <header>{props.issue.title}</header>
+            <span className="text-gray-500 text-sm">
+              {dateFormatter.format(new Date(props.issue.date))}
+            </span>
+          </div>
+        </Link>
+      </li>
+    </>
+  );
+};
+
+//#endregion
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { RefObject, useRef, useState } from 'react';
-import { RootSidebar } from './shared/components/root-sidebar';
+import { NewIssueSVG } from '../shared/components/svgs/new-issue';
+import { SearchSVG } from '../shared/components/svgs/search';
+import { InboxSVG } from '../shared/components/svgs/inbox-svg';
+import { IssuesSVG } from '../shared/components/svgs/issues';
+import { ViewsSVG } from '../shared/components/svgs/views';
 
 export const Root = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
@@ -33,7 +37,6 @@ export const Root = () => {
   const [isNavVisible, setIsNavVisible] = useState(false);
   const handleIsNavVisibleClick = () => {
     setIsNavVisible((prevState) => !prevState);
-    console.log('Nav visibility toggled:', !isNavVisible);
   };
 
   return (
@@ -50,6 +53,7 @@ export const Root = () => {
               dialogInnerRef,
               dialogRef,
               handleIsNavVisibleClick,
+              setIsNavVisible,
             }}
           />
         </div>
@@ -69,4 +73,137 @@ export type OutletContext = {
   dialogRef: RefObject<HTMLDialogElement>;
   dialogInnerRef: RefObject<HTMLDivElement>;
   handleIsNavVisibleClick: () => void;
+  setIsNavVisible: (value: boolean) => void;
 };
+
+//#region MARK: RootSidebar
+type RootSidebarProps = {
+  openDialog: () => void;
+  isNavVisible: boolean;
+};
+const RootSidebar = (props: RootSidebarProps) => {
+  return (
+    <>
+      <div className="lg:w-[255px] md:0px"></div>
+      <div
+        className={`nav-wrapper left-0 top-0 bottom-0 w-[255px] fixed ${
+          props.isNavVisible ? 'nav-visible' : ''
+        } `}
+      >
+        <nav
+          className={` min-w-[220px] max-w-[255px] h-dvh border-0 border-r border-solid border-gray-300 text-sm nav-global bg-white flex flex-col relative `}
+        >
+          <header className="flex gap-1 justify-between items-center px-4 min-[360px]:pt-10 lg:pt-3 pb-3">
+            <button className="hover:bg-[#e1e1e1]">
+              <div className="p-1">
+                <div className="bg-green-600 text-white rounded-md ">
+                  <span className="px-1 ">O</span>
+                </div>
+              </div>
+            </button>
+
+            <a
+              className="hover:bg-[#e1e1e1] rounded-md leading-6 px-2"
+              href="https://github.com/otabekshoyimov/not-linear"
+            >
+              github
+            </a>
+          </header>
+          <main className="pt-2">
+            <div className="px-4">
+              <button
+                onClick={props.openDialog}
+                className="hover:bg-[#e1e1e1] hover:rounded-md flex items-center gap-2 mb-6 w-full outline outline-1 outline-gray-300 rounded-md shadow-sm p-1 pl-2 "
+              >
+                <NewIssueSVG
+                  name="NewIssue"
+                  width={16}
+                  height={16}
+                  className="group-hover:text-black text-[#575859]"
+                />
+                New issue
+              </button>
+            </div>
+            <ul className="flex flex-col gap-1 px-4 mb-5">
+              <li className="p-1 pl-2 hover:bg-[#e1e1e1] hover:rounded-md flex items-center gap-2 outline outline-1 outline-gray-300 rounded-md group ">
+                <button className="w-full flex gap-2">
+                  <SearchSVG
+                    name="Search"
+                    width={16}
+                    height={16}
+                    className="group-hover:text-black text-[#575859]"
+                  />
+                  Search
+                </button>
+              </li>
+              <li className="p-1 pl-2 hover:bg-[#e1e1e1] hover:rounded-md items-center  group ">
+                <a href="" className="w-full flex gap-2">
+                  <InboxSVG
+                    name="Inbox"
+                    width={16}
+                    height={16}
+                    className="group-hover:text-black text-[#575859]"
+                  />
+                  Inbox
+                </a>
+              </li>
+              <li className="p-1 pl-2 hover:bg-[#e1e1e1] hover:rounded-md flex items-center gap-2 group ">
+                <a href="" className="flex gap-2 w-full">
+                  <IssuesSVG
+                    name="Issues"
+                    width={16}
+                    height={16}
+                    className="group-hover:text-black text-[#575859]"
+                  />
+                  Issues
+                </a>
+              </li>
+              <li className="p-1 pl-2 hover:bg-[#e1e1e1] hover:rounded-md flex items-center gap-2  group">
+                <a href="" className="flex gap-2 w-full">
+                  <ViewsSVG
+                    name="Views"
+                    width={16}
+                    height={16}
+                    className="group-hover:text-black text-[#575859]"
+                  />
+                  Views
+                </a>
+              </li>
+            </ul>
+            <details className="px-4">
+              <summary className="hover:bg-gray-300 rounded-md px-3 leading-6">
+                Your team
+              </summary>
+              <ul className="mt-4">
+                <li className="hover:bg-[#e1e1e1] hover:rounded-md group ">
+                  <a href="" className="flex items-center gap-3 pl-6 h-7">
+                    <InboxSVG
+                      name="Inbox"
+                      width={16}
+                      height={16}
+                      className="text-[#575859] group-hover:text-black"
+                    />
+                    Issues
+                  </a>
+                </li>
+                <div className="ml-8 pl-4 border-0 border-l border-solid border-gray-300 flex flex-col gap-1">
+                  <li className=" hover:bg-[#e1e1e1] hover:rounded-md">
+                    <a href="" className="flex items-center gap-2 h-7 pl-1">
+                      Active
+                    </a>
+                  </li>
+                  <li className=" hover:bg-[#e1e1e1] hover:rounded-md">
+                    <a href="" className="flex items-center gap-2 h-7 pl-1">
+                      Backlog
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </details>
+          </main>
+        </nav>
+      </div>
+    </>
+  );
+};
+//#endregion
