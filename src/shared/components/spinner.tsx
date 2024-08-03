5cf8a13be30a3049b3530494e806b360936cf2b0
otabek <95689252+otabek-git@users.noreply.github.com>
2024-08-03 18:07:44 +0500

    fix: spinner size

--- src/shared/components/spinner.tsx
+++ src/shared/components/spinner.tsx
@@ -3,7 +3,7 @@ import { RefreshCw } from 'lucide-react';
 export const Spinner = () => {
   return (
     <>
-      <RefreshCw className="animate-spin" color="#7a7a7a" size={16} />
+      <RefreshCw className="animate-spin" color="#7a7a7a" size={20} />
     </>
   );
 };
