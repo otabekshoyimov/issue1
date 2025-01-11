3467d0d18c654216501cf7ebf5d75fd8a26ac6a0
otabekshoyimov <95689252+otabekshoyimov@users.noreply.github.com>
2025-01-11 18:59:15 +0500

    refactor(error-page): remove 'any' type casting

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,50 +1,35 @@
-import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const error = useRouteError() as any;
+  const error = useRouteError() ;
   console.error({ error });
+  console.log(isRouteErrorResponse(error))
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="flex min-h-screen w-full flex-col items-center justify-center px-20 bg-green-300">
+            <h2>{error.status} {error.statusText}</h2>
+            <p className="pb-2">{error.data}</p>
+            <span className="pb-20 text-center ">
+              "Sorry, we can't find the page you are looking for"
+            </span>
+      </div>
+    );
+  }
 
   if (error instanceof Error) {
     return (
       <section className="flex min-h-screen w-full flex-col items-center justify-center">
         <div>
-          <h2 className="pb-4 underline decoration-red-500 decoration-wavy">
-            Application error
+          <h2 className="pb-2 underline decoration-red-500 decoration-wavy">
+           Error
           </h2>
-          <p>{error.message}</p>
-    
-          <Link to={"/"}>Return home →</Link>
+          <p className="pb-10">{error.message}</p>
         </div>
       </section>
     );
   }
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div className="flex min-h-screen w-full flex-col items-center justify-center px-20">
-        {error.status === 404 ? (
-          <>
-            <h2>{error.status}</h2>
-            <p>{error.statusText}</p>
-            <span className="pb-20 text-center underline decoration-red-500 decoration-wavy">
-              "Sorry, we can't find the page you are looking for"
-            </span>
-
-            <Link to="/">
-              <span>Return to homepage →</span>
-            </Link>
-          </>
-        ) : (
-          <>
-            <h2>Error: {error.status}</h2>
-            <p>{error.statusText}</p>
-            <p>{error.data}</p>
-          </>
-        )}
-      </div>
-    );
-  }
 
   return (
     <div>
