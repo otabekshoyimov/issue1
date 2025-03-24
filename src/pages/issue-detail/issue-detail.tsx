d00e61742f70b1d296e55c1d170f0749a2ab1c20
otabekshoyimov <95689252+otabekshoyimov@users.noreply.github.com>
2025-03-24 21:22:13 +0500

    Cleanup

--- src/pages/issue-detail/issue-detail.tsx
+++ src/pages/issue-detail/issue-detail.tsx
@@ -1,23 +1,20 @@
 import type { Params } from "react-router-dom";
 import { useLoaderData, useNavigation } from "react-router-dom";
+import { IssuesHeader } from "../../features/issue/issues-list/ui/issues-header";
 import { pocketbase } from "../../shared/api/pocketbase";
 import { Spinner } from "../../shared/ui/spinner";
-import { IssuesHeader } from "../../features/issue/issues-list/ui/issues-header";
 import { format_date } from "../../shared/utils/date/format-date";
 
 export async function issue_detail_loader({ params }: { params: Params }) {
-  console.log("Params:", params, typeof params.issueId);
   if (!params.issueId) {
     throw new Error("issue id is required");
   }
   const issue = await pocketbase.collection("posts").getOne(params.issueId);
-  console.log("Fetched Issue:", issue);
   return issue;
 }
 
 export const IssueDetail = () => {
   const issue = useLoaderData() as any;
-  console.log(issue);
   const navigation = useNavigation();
   if (navigation.state === "loading") {
     return (
